3858423f00d4ce3bcdc4a5604bf8d24ffe50d1e2
BlueEagle421 <joachim.wielewski@gmail.com>
2025-09-24 09:12:13 +0200

    Remove mod settings

--- Source/Meta/Mod.cs
+++ Source/Meta/Mod.cs
@@ -7,13 +7,10 @@ namespace USH_HE;
 
 public class HE_Mod : Mod
 {
-    public static HE_Settings Settings { get; private set; }
 
     public HE_Mod(ModContentPack content) : base(content)
     {
         InitHarmony();
-
-        Settings = GetSettings<HE_Settings>();
     }
 
     private void InitHarmony()
@@ -21,8 +18,4 @@ public class HE_Mod : Mod
         Harmony harmony = new("HackingExpansion");
         harmony.PatchAll(Assembly.GetExecutingAssembly());
     }
-
-    public override void DoSettingsWindowContents(Rect inRect) => Settings.DoSettingsWindowContents(inRect);
-
-    public override string SettingsCategory() => "Hacking Expansion";
 }
