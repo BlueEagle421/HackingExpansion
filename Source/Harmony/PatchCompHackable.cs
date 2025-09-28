8c669ec8114e2edfb261f862ad6fdadff1a490d2
BlueEagle421 <joachim.wielewski@gmail.com>
2025-09-28 18:30:01 +0200

    Add faction check patch

--- Source/Harmony/PatchCompHackable.cs
+++ Source/Harmony/PatchCompHackable.cs
@@ -3,44 +3,59 @@ using HarmonyLib;
 using RimWorld;
 using Verse;
 
-namespace USH_HE
+namespace USH_HE;
+
+[HarmonyPatch(typeof(CompHackable), nameof(CompHackable.Hack))]
+public static class Patch_CompHackable_Hack
 {
-    [HarmonyPatch(typeof(CompHackable), nameof(CompHackable.Hack))]
-    public static class Patch_CompHackable_Hack
-    {
-        private static readonly ConditionalWeakTable<CompHackable, CompDataSource> _cache = [];
+    private static readonly ConditionalWeakTable<CompHackable, CompDataSource> _cache = [];
 
-        static void Postfix(CompHackable __instance, float amount, Pawn hacker = null)
+    static void Postfix(CompHackable __instance, float amount, Pawn hacker = null)
+    {
+        if (__instance == null)
+            return;
+
+        if (!_cache.TryGetValue(__instance, out var compDataSource))
         {
-            if (__instance == null)
+            compDataSource = __instance.parent.GetComp<CompDataSource>();
+
+            if (compDataSource == null)
                 return;
 
-            if (!_cache.TryGetValue(__instance, out var compDataSource))
-            {
-                compDataSource = __instance.parent.GetComp<CompDataSource>();
-
-                if (compDataSource == null)
-                    return;
-
-                _cache.Add(__instance, compDataSource);
-            }
-
-            compDataSource.Hack(amount, hacker);
+            _cache.Add(__instance, compDataSource);
         }
+
+        compDataSource.Hack(amount, hacker);
     }
+}
 
-    [HarmonyPatch(typeof(CompHackable), "ProcessHacked")]
-    public static class Patch_CompHackable_ProcessHack
+[HarmonyPatch(typeof(CompHackable), "ProcessHacked")]
+public static class Patch_CompHackable_ProcessHack
+{
+    static void Postfix(CompHackable __instance, Pawn hacker, bool suppressMessages)
     {
-        static void Postfix(CompHackable __instance, Pawn hacker, bool suppressMessages)
+        if (__instance == null)
+            return;
+
+        if (!__instance.parent.TryGetComp(out CompDataSource compDataSource))
+            return;
+
+        compDataSource.ProcessHacked(hacker, suppressMessages);
+    }
+}
+
+[HarmonyPatch(typeof(CompHackable), nameof(CompHackable.CanHackNow), [typeof(Pawn)])]
+public static class Patch_CompHackable_CanHackNow
+{
+    static void Postfix(CompHackable __instance, ref AcceptanceReport __result, Pawn pawn)
+    {
+        if (!__result.Accepted)
+            return;
+
+        if (__instance?.parent?.Faction != null && pawn?.Faction != null
+            && __instance.parent.Faction == pawn.Faction)
         {
-            if (__instance == null)
-                return;
-
-            if (!__instance.parent.TryGetComp(out CompDataSource compDataSource))
-                return;
-
-            compDataSource.ProcessHacked(hacker, suppressMessages);
+            __result = "USH_HE_BelongsToHacker".Translate();
         }
     }
 }
